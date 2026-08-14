689f50618e77e7f3af4ed5bc0d7649eb3ba10a4a
Sfiggordon <lihanovigor86@gmail.com>
2026-08-14 12:25:13 +0500

    Update ItemCarryInit.java

--- src/main/java/com/igor/itemcarry/ItemCarryInit.java
+++ src/main/java/com/igor/itemcarry/ItemCarryInit.java
@@ -3,12 +3,16 @@ package com.igor.itemcarry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.util.Identifier;
 
 public class ItemCarryInit implements ModInitializer {
+    public static final Identifier PICKUP_CHANNEL = new Identifier("itemcarry", "pickup");
+    public static final Identifier CARRY_TOGGLE_CHANNEL = new Identifier("itemcarry", "carry_toggle");
+
     @Override
     public void onInitialize() {
-        // Feature 1 & 2: Items never despawn
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
             if (entity instanceof ItemEntity item) {
                 item.setNeverDespawn();
@@ -16,9 +20,18 @@ public class ItemCarryInit implements ModInitializer {
             }
         });
 
-        // Feature 3: Update carried items
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             ItemCarryMod.onServerTick(server);
+        });
+
+        ServerPlayNetworking.registerGlobalReceiver(PICKUP_CHANNEL, (server, player, handler, buf, responseSender) -> {
+            int entityId = buf.readInt();
+            server.execute(() -> ItemCarryMod.handlePickup(player, entityId));
+        });
+
+        ServerPlayNetworking.registerGlobalReceiver(CARRY_TOGGLE_CHANNEL, (server, player, handler, buf, responseSender) -> {
+            int entityId = buf.readInt();
+            server.execute(() -> ItemCarryMod.handleCarryToggle(player, entityId));
         });
     }
 }
