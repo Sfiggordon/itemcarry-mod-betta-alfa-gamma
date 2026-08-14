ea901e2d55b6d311a37d83be4abe937c129a9f5d
Sfiggordon <lihanovigor86@gmail.com>
2026-08-14 14:07:05 +0500

    Update ItemCarryInit.java

--- src/main/java/com/igor/itemcarry/ItemCarryInit.java
+++ src/main/java/com/igor/itemcarry/ItemCarryInit.java
@@ -3,7 +3,6 @@ package com.igor.itemcarry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.util.Identifier;
 
@@ -22,16 +21,6 @@ public class ItemCarryInit implements ModInitializer {
 
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             ItemCarryMod.onServerTick(server);
-        });
-
-        ServerPlayNetworking.registerGlobalReceiver(PICKUP_CHANNEL, (server, player, handler, buf, responseSender) -> {
-            int entityId = buf.readInt();
-            server.execute(() -> ItemCarryMod.handlePickup(player, entityId));
-        });
-
-        ServerPlayNetworking.registerGlobalReceiver(CARRY_TOGGLE_CHANNEL, (server, player, handler, buf, responseSender) -> {
-            int entityId = buf.readInt();
-            server.execute(() -> ItemCarryMod.handleCarryToggle(player, entityId));
         });
     }
 }
