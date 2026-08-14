c00868280f14abac8b188f332d0c9ab72747d30e
Sfiggordon <lihanovigor86@gmail.com>
2026-08-14 12:11:24 +0500

    Update ItemCarryInit.java

--- src/main/java/com/igor/itemcarry/ItemCarryInit.java
+++ src/main/java/com/igor/itemcarry/ItemCarryInit.java
@@ -1,10 +1,24 @@
 package com.igor.itemcarry;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.ItemEntity;
 
 public class ItemCarryInit implements ModInitializer {
     @Override
     public void onInitialize() {
-        System.out.println("ItemCarry Mod loaded!");
+        // Feature 1 & 2: Items never despawn
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (entity instanceof ItemEntity item) {
+                item.setNeverDespawn();
+                item.setPickupDelayInfinite();
+            }
+        });
+
+        // Feature 3: Update carried items
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            ItemCarryMod.onServerTick(server);
+        });
     }
 }
