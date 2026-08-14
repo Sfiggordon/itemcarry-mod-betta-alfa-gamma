080d4e89427ed590ab6b9ba6225613b638333bc2
Sfiggordon <lihanovigor86@gmail.com>
2026-08-14 12:02:09 +0500

    Update ItemCarryInit.java

--- src/main/java/com/igor/itemcarry/ItemCarryInit.java
+++ src/main/java/com/igor/itemcarry/ItemCarryInit.java
@@ -1,17 +1,10 @@
 package com.igor.itemcarry;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.ItemEntity;
 
 public class ItemCarryInit implements ModInitializer {
     @Override
     public void onInitialize() {
-        // Используем event hook из Minecraft для обработки спаун айтемов
-        net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-            if (entity instanceof ItemEntity item) {
-                item.setNeverDespawn();
-                item.setPickupDelayInfinite();
-            }
-        });
+        System.out.println("ItemCarry Mod loaded!");
     }
 }
