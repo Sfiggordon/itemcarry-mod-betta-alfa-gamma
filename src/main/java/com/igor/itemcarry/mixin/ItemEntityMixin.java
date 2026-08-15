8526cc1e2c01bcb9a473a3d6af146b87ba05f117
Sfiggordon <lihanovigor86@gmail.com>
2026-08-15 10:40:03 +0500

    Update ItemEntityMixin.java

--- src/main/java/com/igor/itemcarry/mixin/ItemEntityMixin.java
+++ src/main/java/com/igor/itemcarry/mixin/ItemEntityMixin.java
@@ -1,15 +1,18 @@
 package com.igor.itemcarry.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ItemEntity.class)
+@Mixin(Entity.class)
 public class ItemEntityMixin {
     @Inject(method = "canHit", at = @At("HEAD"), cancellable = true)
     private void itemcarry$canHit(CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
+        if ((Object) this instanceof ItemEntity) {
+            cir.setReturnValue(true);
+        }
     }
 }
