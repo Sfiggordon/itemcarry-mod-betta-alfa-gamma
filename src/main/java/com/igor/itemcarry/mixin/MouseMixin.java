d5ca9480596243c77d6d9a85dbb3fae49baf2b2b
Sfiggordon <lihanovigor86@gmail.com>
2026-08-16 10:21:38 +0500

    Update MouseMixin.java

--- src/main/java/com/igor/itemcarry/mixin/MouseMixin.java
+++ src/main/java/com/igor/itemcarry/mixin/MouseMixin.java
@@ -2,6 +2,7 @@ package com.igor.itemcarry.mixin;
 
 import com.igor.itemcarry.ItemCarryClient;
 import net.minecraft.client.Mouse;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,6 +14,14 @@ public class MouseMixin {
     private void itemcarry$onScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
         if (ItemCarryClient.isCarrying()) {
             ItemCarryClient.adjustCarryDistance(vertical);
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "onMouseButton", at = @At("HEAD"), cancellable = true)
+    private void itemcarry$onButton(long window, int button, int action, int mods, CallbackInfo ci) {
+        if (ItemCarryClient.isCarrying() && button == GLFW.GLFW_MOUSE_BUTTON_RIGHT && action == GLFW.GLFW_PRESS) {
+            ItemCarryClient.onRightClickPlace();
             ci.cancel();
         }
     }
